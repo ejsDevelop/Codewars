5c7554b6dda2d25881d10e80e6fedc58dddb5d35
Erik Schjolberg <erik.schjolberg@gmail.com>
2019-04-20 08:33:12 -0500

    commit

---
+++ reversedSequence.js
@@ -0,0 +1,12 @@
+/*
+Get the number n (n>0) to return the reversed sequence from n to 1.
+
+Example : n=5 >> [5,4,3,2,1]
+
+FUNDAMENTALS
+*/
+
+const reverseSeq = n => {
+    return Array(n).fill(0).map((e, i) => n - i );
+  };
+
