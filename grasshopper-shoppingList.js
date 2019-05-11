2e4d1b310351bb29d027599a287bf73083cf4aca
Erik Schjolberg <erik.schjolberg@gmail.com>
2019-05-11 14:13:18 -0500

    commit

--- grasshopper-shoppingList.js
+++ grasshopper-shoppingList.js
@@ -15,8 +15,16 @@ frenchFries
 Create a variable named totalPrice that finds the cost of all of the food.
 */
 
+let [sandwiches, salads, wraps, frenchFries]  = [4, 6, 5, 10],
+    totalPrice = (sandwiches * 8.00) + (salads * 7.00) + (wraps * 6.50) + (frenchFries * 1.20);
+
+/* 
+or 
+
 const sandwiches=4
 const salads=6
 const wraps=5
 const frenchFries=10
 const totalPrice=(sandwiches+salads+wraps+frenchFries)*4.74
+
+*/
