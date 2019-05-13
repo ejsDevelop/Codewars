e4d62f09c01929b624b61e9a3a10223f6f51ac7e
Erik Schjolberg <erik.schjolberg@gmail.com>
2019-05-13 12:14:13 -0500

    commit

---
+++ tripleDouble.js
@@ -0,0 +1,21 @@
+/*
+Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+Ex) Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+Note: You can expect all of the inputs to be the same length.
+*/
+
+var result = "";
+for (let i = 0; i < one.length; i++) {
+  result += one.charAt(i) + two.charAt(i) + three.charAt(i);
+}
+return result;
+}
+
+//or
+
+function tripleTrouble(one, two, three){
+    return one.split('').reduce((a,s,c)=>`${a}${one[c]}${two[c]}${three[c]}`,'')
+ }
+
