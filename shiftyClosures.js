76cecb865053e0247f176f57b9b1c67243343558
Erik Schjolberg <erik.schjolberg@gmail.com>
2019-05-18 18:15:42 -0500

    commit x

--- shiftyClosures.js
+++ shiftyClosures.js
@@ -1,7 +1,29 @@
-/*
+/* 
 Functional closures can get overly attached. Set them straight!
 
 Why doesn't greet_abe() actually greet Abe?
 */
 
+var greet_abe = function() {
+    var name = 'Abe';
+      return "Hello, " + name + '!';
+    };
+    
+    var greet_ben = function() {
+    var name = 'Ben';
+      return "Hello, " + name + '!';
+    };
 
+//or
+
+var name;
+function greet_abe() {
+  name = 'Abe';
+  return "Hello, " + name + '!';
+};
+
+
+function greet_ben() {
+  name = 'Ben'; 
+  return "Hello, " + name + '!';
+};
