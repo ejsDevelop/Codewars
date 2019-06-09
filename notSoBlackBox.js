9f94bedff554a24e0ea80a0222ca84e674e0548a
Erik Schjolberg <erik.schjolberg@gmail.com>
2019-06-09 12:24:00 -0500

    x

---
+++ notSoBlackBox.js
@@ -0,0 +1,13 @@
+/*
+Description:
+The kata has been preloaded with a function called blackBox. Figure out what the function does.
+You may need to think laterally!
+*/
+
+//Do console.log(blackBox.toString());
+
+JSopenSesame();
+
+
+
+
